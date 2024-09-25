ba9e624d7c25b8f26215184e513f0ff4bcb354a6
Raman Randive <ramanrandive2004@gmail.com>
2024-09-25 20:59:19 +0530

    card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,19 +1,19 @@
 import React from 'react'
-import Button from './Lang_Button'
+// import Button from './Lang_Button'
 import './css/card.css'
 const Card = (props) => {
   return (
-    <div className='card' >
-        <img className='pic'src={props.image} alt={props.imgd} />
-        <h1>{props.title}</h1>
+    <div className="card" id={props.key}>
+    <img src={props.image} alt="..." className="card-image" />
+    <div className="card-content">
+        <h2 className="card-title">{props.title}</h2>
         <details>
-            <summary>{props.summary}</summary>
-            <p>{props.details}</p>
+        <summary>{props.summary}</summary>  
+        <p className="card-description">{props.description}</p>
         </details>
-        <Button/>
-        <p>{props.pull_count}</p>
-        <button>Register</button>
+        <button className="card-button">{props.pull_count}</button>
     </div>
+</div>
   )
 }
 
--- src/components/Note.js
+++ src/components/Note.js
@@ -1,18 +1,61 @@
 import React from 'react';
 import Card from './Card';
-import i1 from '../assets/pikachu.jpg'
+import { useState, useEffect } from 'react';
+
 const Note = () => {
-  return (
-    <div>
-      <Card
-        image={i1}
-        title="Hello"
-        summary="dfkjdfnjfddfsj"
-        details="dfsjdfsjlefdfd"
-        pull_count="fsiudf"
-      />
-    </div>
-  );
+
+  // const [cards, setcard] = useState([]);
+
+  // useEffect( ()=>{
+  //   const fetchdata = async ()=>{
+  //       const response = await fetch('./data.json');
+  //       const data = response.json();
+  //       setcard(data);
+  //     }
+  //   fetchdata();
+  // }, [])
+
+  
+
+  // return (
+  //   <div>
+  //     {cards.map((data, index) => (
+  //   <Card
+  //   key = {index}
+  //   image={data.image}
+  //   title={data.title}
+  //   summary={data.summary}
+  //   description={data.description}
+  //   pull_count={data.pull_count}
+  // />))}
+  
+  //   </div>
+  // );
+  const [cards, setCards] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch('data.json'); // Adjust the path if needed
+            const data = await response.json();
+            setCards(data);
+        };
+        fetchData();
+    }, []);
+
+    return (
+        <div>
+            {cards.map((data, index) => (
+                <Card
+                    key={index}
+                    image={data.image}
+                    title={data.title}
+                    summary={data.summary}
+                    description={data.description}
+                    pull_count={data.pull_count}
+                />
+            ))}
+        </div>
+    );
 }
 
 export default Note;
